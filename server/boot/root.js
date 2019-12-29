91d3a4c3bdf9faafc0a4b2d2849704d04d2794ca
root <root@localhost.localdomain>
2019-12-29 11:44:54 +0000

    deploy 1

--- server/boot/root.js
+++ server/boot/root.js
@@ -5,27 +5,28 @@ module.exports = function(server) {
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
   server.use(router);
-};
 
 
-User.create([
+server.models.User.create([
     {username: 'admin1', email: 'guynkama@gmail.com', password: 'TruePass4'}
 ], function(err, users) {
     if (err) return debug('%j', err);
     // Create the admin role
-    Role.create({
+    server.models.Role.create({
       name: 'admin'
     }, function(err, role) {
       if (err) return debug(err);
-      debug(role);
+      console.log(role);
 
       // Make admin1 an admin
       role.principals.create({
-        principalType: RoleMapping.USER,
+        principalType: server.models.RoleMapping.USER,
         principalId: users[0].id
       }, function(err, principal) {
         if (err) return debug(err);
-        debug(principal);
+        console.log(principal);
       });
     });
   });
+};
+
