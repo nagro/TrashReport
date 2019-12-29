96ae14df0b9686a47ffb736dc244127a8cabe1e4
guy <carlbenji4@gmail.com>
2019-12-29 10:26:04 +0100

    auto migrate models

--- server/boot/root.js
+++ server/boot/root.js
@@ -6,3 +6,26 @@ module.exports = function(server) {
   router.get('/', server.loopback.status());
   server.use(router);
 };
+
+
+User.create([
+    {username: 'admin1', email: 'guynkama@gmail.com', password: 'TruePass4'}
+], function(err, users) {
+    if (err) return debug('%j', err);
+    // Create the admin role
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) return debug(err);
+      debug(role);
+
+      // Make admin1 an admin
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[0].id
+      }, function(err, principal) {
+        if (err) return debug(err);
+        debug(principal);
+      });
+    });
+  });
